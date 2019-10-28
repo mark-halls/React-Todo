42762d28f22d86776a12f2bae037998667a8ff78
Mark Halls <mark.halls@gmail.com>
2019-10-28 14:18:39 -0700

    Add todolist component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+
+import Todo from "./Todo";
+
+class TodoList extends React.Component {
+  render() {
+    <div className="todo-list">
+      {this.props.todo.map(task => (
+        <Todo
+          key={item.id}
+          task={task}
+          toggleCompleted={this.props.toggleCompleted}
+        />
+      ))}
+    </div>;
+  }
+}
+
+export default TodoList;
