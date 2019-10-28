da59a367886ab3a8390ef136c8e8cf39d38c6252
Mark Halls <mark.halls@gmail.com>
2019-10-28 14:07:15 -0700

    Add todo component

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Todo extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    <div>
+      <p>{this.props.task}</p>;
+    </div>;
+  }
+}
+
+export default Todo;
